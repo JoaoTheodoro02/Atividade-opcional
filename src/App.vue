ce9c1c9331f36dab695b99fb33af685ff92e3114
JoaoTheodoro02 <joao.theodoro@unemat.br>
2025-04-22 19:20:14 -0400

    Ok

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app>
+    <h1 style="text-align: center; margin-top: 40px; color: blue;">
+      O VUE FUNCIONOU
+    </h1>
+    <router-view />
+  </v-app>
+</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,40 @@
+<!-- layouts/default.vue -->
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <Menu v-if="userStore.isLoggedIn" />
+      <template #append>
+        <div class="pa-2">
+          <v-btn block to="/login">
+            Login
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-app-bar-title>IntelliSystem</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Menu from '@/components/Menu.vue'
+import { useUserStore } from '@/stores/user.js'
+
+const drawer = ref(null)
+const userStore = useUserStore()
+
+// Restaura o estado de login ao iniciar o app
+onMounted(() => {
+  userStore.init()
+})
+</script>
